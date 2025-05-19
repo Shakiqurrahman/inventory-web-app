4cc60080dd2ba07fd61aedb0fb3c0acc529d3986
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-05-19 23:27:08 +0600

    layout routing setup completed

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
+import ErrorPage from "../pages/ErrorPage";
+import LoginPage from "../pages/LoginPage";
+import MainLayout from "./Layout";
 
 const routes: RouteObject[] = [
     {
         path: "/",
-        element: <div>Hello World</div>,
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <div>Hello World</div>,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "*",
+        element: <ErrorPage />,
     },
 ];
 
