c6dd9d1d244f3b4a8cc9d0599faa3f7945cf8160
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-05-28 01:12:53 +0600

    Supplier page , create supplier page design complete

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,35 +4,45 @@ import ErrorPage from "../pages/ErrorPage";
 import LoginPage from "../pages/LoginPage";
 import ResetPasswordOtp from "../pages/resetPassword/ResetPasswordOtp";
 import ResetPasswordPage from "../pages/resetPassword/ResetPasswordPage";
+import NewSuplier from "../pages/supplierPage/NewSuplier";
+import SuppliersPage from "../pages/supplierPage/SuppliersPage";
 import MainLayout from "./Layout";
 
 const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
+    {
         path: "/",
-        element: <DashboardPage />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPasswordPage />,
-  },
-  {
-    path: "reset-password/otp",
-    element: <ResetPasswordOtp />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <DashboardPage />,
+            },
+            {
+                path: "/suppliers",
+                element: <SuppliersPage />,
+            },
+            {
+                path: "/suppliers/new-supplier",
+                element: <NewSuplier />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/reset-password",
+        element: <ResetPasswordPage />,
+    },
+    {
+        path: "reset-password/otp",
+        element: <ResetPasswordOtp />,
+    },
+    {
+        path: "*",
+        element: <ErrorPage />,
+    },
 ];
 
 export const router = createBrowserRouter(routes);
