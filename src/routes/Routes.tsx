28eb5bad181a7b094164e863fcf4f798844b7730
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-05-30 23:02:01 +0600

    creating new password route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import Customerspage from "../pages/customerPage/Customerspage";
 import DashboardPage from "../pages/DashboardPage";
 import ErrorPage from "../pages/ErrorPage";
 import LoginPage from "../pages/LoginPage";
+import NewPassword from "../pages/resetPassword/NewPassword";
 import ResetPasswordOtp from "../pages/resetPassword/ResetPasswordOtp";
 import ResetPasswordPage from "../pages/resetPassword/ResetPasswordPage";
 import NewSuplier from "../pages/supplierPage/NewSuplier";
@@ -43,6 +44,10 @@ const routes: RouteObject[] = [
     {
         path: "reset-password/otp",
         element: <ResetPasswordOtp />,
+    },
+    {
+        path: "new-password",
+        element: <NewPassword />,
     },
     {
         path: "*",
